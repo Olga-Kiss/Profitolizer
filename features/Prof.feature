cbbe92c55a6387bc8fe425845dc6669c57187fc3
Olga K. <olga_kisilyova@yahoo.com>
2024-10-09 18:43:46 -0600

    first commit

---
+++ features/Prof.feature
@@ -0,0 +1,76 @@
+Feature: Test login functionality
+
+  Scenario: Login happy path
+    Given Olga open prod environment
+    Then Olga login with Wrong credentials
+    Then Verify page by title "Profotolizer - Projects"
+
+  Scenario: Login with wrong credentials
+    Given Olga open prod environment
+    Then Olga login with Wrong credentials
+    Then Verify presents of element "//p[text() = 'Invalid username or password']"
+
+  Scenario: Login without username
+    Given Olga open prod environment
+    Then Olga login with Empty_email credentials
+    Then Verify presents of element "//div[text() = 'Email is required']"
+
+  Scenario: Login without password
+    Given Olga open prod environment
+    Then Olga login with Empty_psw credentials
+    Then Verify presents of element "//div[text() = 'Password is required']"
+
+  Scenario: Recreate the password
+    Given Olga open prod environment
+    Then Click element "//a[text()='Login']"
+    Then Wait 1 seconds
+    Then Click element "//a[text()='Forgot password?']"
+    Then Wait 1 seconds
+    Then Type "ratovbj@htmail.store" into "//input[@class='el-input__inner']"
+    Then Wait 1 seconds
+    Then Click element "//button[contains(text(), 'Send')]"
+    Then Wait 2 seconds
+    Then Verify presents of element "//h5[contains(text(), 'Please check your inbox.')]"
+
+  Scenario: Creating a Project
+    Given Olga open prod environment
+    Then Olga login with Admin credentials
+    Then Verify page by title "Profotolizer - Projects"
+    #Then Click element "//label[text()='Project Name']/following-sibling::div//input[@id=//label[text()='Project Name']/@for]/ancestor::div[@class='el-input__wrapper']"
+    Then Wait 1 seconds
+    Then Type "Example" into "//input[@class='el-input__inner'][not(contains(@placeholder, 'Start date'))]"
+    Then Wait 1 seconds
+    Then Click element "//input[@placeholder='Start date']"
+    Then Wait 1 seconds
+    Then Type "2024-10-05" into "//input[@class='el-input__inner'][contains(@placeholder, 'Start date')]"
+    Then Wait 1 seconds
+    Then Click element "//span[text()='Period Dimension']"
+    Then Wait 1 seconds
+    Then Click element "//span[text()='Month']"
+    Then Wait 1 seconds
+    Then Click element "//span[text()='2 Years']"
+    Then Wait 1 seconds
+    Then Click element "//button[text()=' Save'][not(contains(@class,'me-2'))]"
+    Then Wait 2 seconds
+
+  Scenario: Creating/Deleting a Project
+    Given Olga open prod environment
+    Then Olga login with Admin credentials
+    Then Wait 2 seconds
+    Then Verify page by title "Profotolizer - Projects"
+    Then Olga fill out following information with keys
+      | key                 | value      |
+      | Project Name        | example    |
+      | Start date          | 2024/10/24 |
+      | Project description | test       |
+      | Period Dimension    | Month      |
+      | Project Duration    | 2 Years    |
+    Then Wait 7 seconds
+    Then Click element "//button[text()=' Save'][not(contains(@class,'me-2'))]"
+    Then Wait 7 seconds
+    Then Click element "//span[text()='Settings']"
+    Then Wait 2 seconds
+    Then Click element "//button[contains(text(),'Delete')]"
+    Then Wait 2 seconds
+    Then Click element "//button[text()='Delete']"
+
---
+++ steps/steps.py
@@ -0,0 +1,103 @@
+from time import sleep
+
+from behave import step
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
+@step('Wait {sec} seconds')
+def wait_sec(context, sec):
+    sleep(int(sec))
+
+
+@step('Click element "{xpath}"')
+def click_element(context, xpath):
+    # element = context.driver.find_element(By.XPATH, xpath)
+
+    element = WebDriverWait(context.driver, 10).until(EC.element_to_be_clickable((By.XPATH, xpath)))
+    element.click()
+
+@step('Type "{text}" into "{xpath}"')
+def type_text(context, text, xpath):
+    element = WebDriverWait(context.driver, 10).until(EC.presence_of_element_located((By.XPATH, xpath)))
+    # element = context.driver.find_element(By.XPATH, xpath)
+    element.send_keys(text)
+
+@step('Olga open "{url}"')
+def olga_open_url(context, url):
+    context.driver.get(url)
+
+@step('Olga open {env} environment')
+def olga_open_env(context, env):
+    envs = {
+        'prod': 'https://www.profitolizer.com/',
+        'dev': 'https://www.dev.profitolizer.com/',
+        'test': 'https://www.test.profitolizer.com/',
+        'uat': 'https://www.uat.profitolizer.com/',
+        'qa': 'https://www.qa.profitolizer.com/',
+    }
+    olga_open_url(context, envs[env])
+
+
+@step('Olga click element "{xpath}"')
+def olga_click_element(context, xpath):
+    # element = context.driver.find_element(By.XPATH, xpath)
+
+    element = WebDriverWait(context.driver, 10).until(EC.element_to_be_clickable((By.XPATH, xpath)))
+    element.click()
+
+@step('Olga type "{text}" into "{xpath}"')
+def olga_type_text(context, text, xpath):
+    element = WebDriverWait(context.driver, 10).until(EC.presence_of_element_located((By.XPATH, xpath)))
+    # element = context.driver.find_element(By.XPATH, xpath)
+    element.send_keys(text)
+
+@step("Olga login with {user} credentials")
+def olga_login(context, user):
+    users = {
+        'Admin': ('ratovbj@htmail.store', '123321'),
+        'Wrong': ('ratovbj@mail.store', '1221'),
+        'Empty_email': ('', '123321'),
+        'Empty_psw': ('ratovbj@htmail.store', '')
+    }
+    olga_click_element(context, "//a[text()='Login']")
+    olga_type_text(context, users[user][0], "//input[@name='username']")
+    olga_type_text(context, users[user][1], "//input[@name='password']")
+    olga_click_element(context, "//button[contains(text(), 'Login')]")
+    sleep(1)
+
+
+@step('Olga verify presents of element "{xpath}"')
+def olga_verify_presents_of_element(context, xpath):
+    elements = context.driver.find_elements(By.XPATH, xpath)
+    assert len(elements) == 1
+
+
+@step("Olga fill out following information with keys")
+def create_project_keys(context):
+    elements = {
+        'Project Name': "//div[./label[text()='Project Name']]//input",
+        'Start date': "//input[@placeholder='Start date']",
+        'Project description': "//div[./label[text()='Project description']]//textarea",
+        'Period Dimension': "//span[text()='Period Dimension']",
+        'Project Duration': "//div[./label[text()='Project Duration']]//span[text()='",
+    }
+    for row in context.table:
+        if row.cells[0] in ['Project Name', 'Start date', 'Project description']:
+            olga_type_text(context, row.cells[1], elements[row.cells[0]])
+        elif row.cells[0] == 'Period Dimension':
+            olga_click_element(context, elements[row.cells[0]])
+            sleep(0.5)
+            olga_click_element(context, f"//li/span[text()='{row.cells[1]}']")
+        else:
+            olga_click_element(context, f"{elements[row.cells[0]]}{row.cells[1]}']")
+
+    @step('Verify page by title "{text}"')
+    def verify_title(context, text):
+        title = context.driver.title
+        assert title == text, f"Expected title: {text}, actual title: {title}. "
+
+    @step('Verify presents of element "{xpath}"')
+    def verify_presents_of_element(context, xpath):
+        elements = context.driver.find_elements(By.XPATH, xpath)
+        assert len(elements) == 1
